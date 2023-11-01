a5db4a02f9cee2cff67b002d8658bba7a9c0878c
vishnupriyads <56652973+vishnupriyads@users.noreply.github.com>
2023-11-02 00:11:37 +0530

    image async load

--- js/custom.js
+++ js/custom.js
@@ -391,3 +391,17 @@ $(function(){
     });
 
 });
+
+function ReLoadImages(){
+  $('img[data-lazysrc]').each( function(){
+      //* set the img src from data-src
+      $( this ).attr( 'src', $( this ).attr( 'data-lazysrc' ) );
+      }
+  );
+}
+
+document.addEventListener('readystatechange', event => {
+  if (event.target.readyState === "interactive") {  //or at "complete" if you want it to execute in the most last state of window.
+      ReLoadImages();
+  }
+});
